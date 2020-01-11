3095ae1a804e564d950adbd7822b405db4166e8c
Loisa-Kitakaya <loisakitakaya@gmail.com>
2020-01-11 17:31:19 +0300

    comments used for better code readability

--- install.sh
+++ install.sh
@@ -1,22 +1,33 @@
 #!/bin/bash
 
+#makes the files executable if they are not
 chmod +x *
 
+#install function
 function install_cc() {
+
+  #create a hidden directory in the user's account folder
+  #copy the contents of this folder to the user's account folder
   sudo mkdir ~/.cc/
   sudo cp * ~/.cc/
 
-  file_list="$(ls)"
-  files=($file_list)
+  #a variable storing the list of items in this folder
+  files="$(ls)"
 
+  #add line to file '.bashrc'
   echo "#Load custom commands" >> ~/.bashrc
 
+  #loop to change the names of the files in the user's account folder
   for file in "${files[@]}"
   do
+
+      #rename files
       sudo mv ~/.cc/$file ~/.cc/.$file
+      #add line to file '.bashrc'
       echo "source ~/.cc/.$file" >> ~/.bashrc
   done
 
+  #instructions to print after installation
   printf "You can now use these commands: \n"
   printf "  'mydistro' to see your OS's distribution info \n  'myhardware' to see your machine's hardware information \n  'make_x_file' to create a file and make it executable \n  'install_cc' to install these custom commands \n"
   printf "Use:'install_cc', to reconfigure the custom commands. \n"
