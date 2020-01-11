ca911a66966890202003becfe4760bde371698fb
Loisa-Kitakaya <loisakitakaya@gmail.com>
2020-01-11 14:33:08 +0300

    comments added for better code readablity

--- hardware-information.sh
+++ hardware-information.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 
 function myhardware() {
+  #info to be displayed on running command
   echo "Infrormation about hardware and running processes:"
-  echo "Options: 'cpu', 'all', 'drives-1', 'drives-2', 'ram'"
+  echo "Options: 'cpu', 'all', 'drives-1', 'drives-2', 'ram', 'system'"
 
+  #take user input
   read answer
 
+  #decision making using the "case" method
   case "$answer" in
-      "cpu") lscpu
+      "cpu") lscpu #shows cpu information
       ;;
-      "all") hwinfo --short
+      "all") hwinfo --short #shows (in summary) information about most of the hardware components of the machine
       ;;
-      "drives-1") df -H
+      "drives-1") df -H #shows information about the drives in the machine
       ;;
-      "drives-2") pydf
+      "drives-2") pydf #shows information about the drives in the machine (in another format)
       ;;
-      "ram") free -m
+      "ram") free -m #shows information about the RAM(s) in the machine
+      ;;
+      "system") dmidecode -t system #shows general information about the machine
       ;;
    esac
 
