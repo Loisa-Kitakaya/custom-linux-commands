db7cc5e1fbaa603f5bdd09b8bddf96445d355252
Loisa-Kitakaya <loisakitakaya@gmail.com>
2020-01-11 20:03:02 +0300

    updated last instruction on reconfiguration

--- install.sh
+++ install.sh
@@ -9,18 +9,18 @@ function install_cc() {
   #install required packages for commands to run
   sudo apt-get install hwinfo pydf
 
-  #create a hidden directory in the user's account folder
-  #copy the contents of this folder to the user's account folder
+  #create a hidden directory in the user's account directory
+  #copy the contents of this directory to the user's account directory
   sudo mkdir ~/.cc/
   sudo cp * ~/.cc/
 
-  #a variable storing the list of items in this folder
+  #a variable storing the list of items in this directory
   files=(create-file-make-it-executable.sh distro-information.sh hardware-information.sh)
 
   #add line to file '.bashrc'
   echo "#Load custom commands" >> ~/.bashrc
 
-  #loop to change the names of the files in the user's account folder
+  #loop to change the names of the files in the user's account directory
   for file in "${files[@]}"
   do
 
@@ -34,7 +34,7 @@ function install_cc() {
   #instructions to print after installation
   printf "You can now use these commands: \n"
   printf "  'mydistro' to see your OS's distribution info \n  'myhardware' to see your machine's hardware information \n  'make_x_file' to create a file and make it executable \n  'install_cc' to install these custom commands \n"
-  printf "Use:'install_cc', to reconfigure the custom commands. \n"
+  printf "Use:'bash install.sh', while inside this directory to reconfigure the custom commands. \n"
 
 }
 
